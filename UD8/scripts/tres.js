f8fa231168c7c23f8723190f471ff1fa4f5b5f43
Sergio Lecuona <serlecu@hotmail.com>
2025-11-05 19:40:59 +0100

    formatear

--- UD8/scripts/tres.js
+++ UD8/scripts/tres.js
@@ -1,31 +1,25 @@
-// capa1 desaparece al arrastrarla sobre capa2
-
 document.addEventListener('DOMContentLoaded', main);
 
 function handleDragCapa1(e) {
-    e.target.style.opacity = 0.5;
+  e.target.style.opacity = 0.5;
 }
 
 function handleDropCapa2(ele, e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if( e.target.id == "capa2") {
-        e.target.style.backgroundColor = "red";
-        e.target.innerHTML= "Lo has logrado";
-        ele.style.display = "none";
-    }
+  if( e.target.id == "capa2") {
+    e.target.style.backgroundColor = "red";
+    e.target.innerHTML= "Lo has logrado";
+    ele.style.display = "none";
+  }
 }
 
-
 function main() {
-    
-    const arrastrable = document.getElementById('capa1');
-    const destino = document.getElementById('capa2');
+  const arrastrable = document.getElementById('capa1');
+  const destino = document.getElementById('capa2');
 
-
-    arrastrable.addEventListener('drag', (e) => handleDragCapa1(e));
-    destino.addEventListener('dragover', (e) => e.preventDefault());
-    destino.addEventListener('drop', (e) => handleDropCapa2(arrastrable,e));
-
+  arrastrable.addEventListener('drag', (e) => handleDragCapa1(e));
+  destino.addEventListener('dragover', (e) => e.preventDefault());
+  destino.addEventListener('drop', (e) => handleDropCapa2(arrastrable,e));
 }
 
