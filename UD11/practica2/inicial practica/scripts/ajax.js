8c97b173f74ff9b09eec9b95853cc2b2a9c90965
Sergio Lecuona <serlecu@hotmail.com>
2025-12-16 09:59:10 +0100

    folder UD11 con índice y  material ini práctica 2

---
+++ UD11/practica2/inicial practica/scripts/ajax.js
@@ -0,0 +1,25 @@
+const BASE_URL = 'https://reqres.in/api/users/';
+const POSTMAN_URL = 'https://httpbin.org/post';
+
+//Código principal dentro del evento load
+// para asegurar la carga de los componentes
+window.addEventListener('load', (ev) => {
+  let numsecs = document.getElementById('numsecs');
+  let user = document.getElementById('user');
+  let boton = document.querySelector('button');
+
+  boton.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    clearFields();
+    procesarFetch(numsecs.value, user.value);
+  });
+});
+
+function clearFields() {
+  document.querySelectorAll('span').forEach((element) => {
+    element.innerHTML = '';
+    console.log(element);
+  });
+}
+
+function procesarFetch(numsecs, user) {}
