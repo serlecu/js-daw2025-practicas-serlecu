265c9e485521ba46622d4fe5634691b4a9ac3c92
Sergio Lecuona <serlecu@hotmail.com>
2025-12-26 18:30:20 +0100

    nueva práctica 2

--- UD11/practica2/scripts/ajax.js
+++ UD11/practica2/scripts/ajax.js
@@ -1,26 +1,5 @@
-const BASE_URL = 'https://reqres.in/api/users/';
-const POSTMAN_URL = 'https://httpbin.org/post';
-
-//Código principal dentro del evento load
-// para asegurar la carga de los componentes
-window.addEventListener('load', (ev) => {
-    let numsecs = document.getElementById('numsecs');
-    let user = document.getElementById('user');
-    let boton = document.querySelector('button');
-
-    boton.addEventListener('click', (ev) => {
-        ev.preventDefault();
-        clearFields();
-        // procesarFetch(numsecs.value, user.value);
-        fetchPost({
-            id: 1,
-            email: 'george.bluth@reqres.in',
-            first_name: 'George',
-            last_name: 'Bluth',
-            avatar: 'https://reqres.in/img/faces/1-image.jpg',
-        });
-    });
-});
+const BASE_URL = 'https://dummyjson.com/products/';
+const POST_URL = 'https://httpbin.org/post';
 
 function clearFields() {
     document.querySelectorAll('span').forEach((element) => {
@@ -29,59 +8,96 @@ function clearFields() {
     });
 }
 
-function fetchPost(data) {
+async function obtenerProducto(prodId) {
+    try {
+        const url = BASE_URL + prodId;
+        const response = await fetch(url);
+        // const data = await response;
+
+        if (!response.ok) {
+            throw new Error(`Error HTTP en GET: ${response.status}`);
+        } else {
+            console.log(`Fetch correcto: ${response.status}`);
+            const data = await response.json();
+            return [data.id, data.title, data.price];
+        }
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+async function ejecutarPOST(data) {
     const postOptions = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: {...data},
-    }
-
-    // el POST
-    fetch('http://httpbin.org/post', postOptions)
-        .then((response) => {
-            if (!response.ok) {
-                document.getElementById('status').innerHTML = response.status;
-                throw new Error(`Error HTTP en POST: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((postData) => {
-            console.log(`POST DATA:`);
-            console.log(postData);
-            document.getElementById('name').innerHTML =
-                postData.data.first_name;
-            document.getElementById('status').innerHTML = 200;
-        })
-        .catch((error) => {
-            console.error('Error en la solicitud POST:', error);
-        });
-}
-
-async function procesarFetch(numsecs, user) {
+        body: JSON.stringify(data),
+      }
+    
     try {
-        const response = await fetch(BASE_URL + user + `?delay=${numsecs}`, {
-            method: 'GET',
-            headers: {
-                // 'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'application/json',
-            },
-            // body: null //JSON.stringify()
-        });
-        if (!response.ok) {
-            throw new Error(`Error HTTP: ${response.status}`);
-        } else {
-            const data = await response.json();
-            const usuarioID = data.id;
-            const usuarioEmail = data.email;
-            document.getElementById('id').innerHTML = usuarioID;
-            document.getElementById('email').innerHTML = usuarioEmail;
-
-            // el POST
-            fetchPost(data);
-        }
+        fetch(POST_URL, postOptions)
+            .then(async (response) => {
+                if (!response.ok) {
+                    document.getElementById('status').innerHTML = response.status;
+                    throw new Error(`Error HTTP en POST: ${response.status}`);
+                } else {
+                    console.log(`Fetch POST correcto: ${response.status}`);
+                }
+                let data = await response.json();
+                return data.json;
+            })
+            .then((postData) => {
+                console.log('Respuesta del POST:', postData);
+                document.getElementById('id_post').innerHTML = postData.id;
+                document.getElementById('title_post').innerHTML = postData.title;
+                document.getElementById('price_post').innerHTML = postData.price;
+                document.getElementById('status').innerHTML = 200;
+            })
+            .catch((error) => {
+                console.error('Error en la solicitud POST:', error);
+            });
     } catch (error) {
         console.error(error);
     }
 }
+
+async function aquireDataPipe(id) {
+    // el GET
+    const data = await obtenerProducto(id);
+
+    if (typeof(data) == null) {
+        document.getElementById('status').innerHTML = data;
+        return;
+    } else {
+        const [id, title, price] = data;
+        document.getElementById('id').innerHTML = id;
+        document.getElementById('title').innerHTML = title;
+        document.getElementById('price').innerHTML = price;
+
+        // el POST
+        await ejecutarPOST({ id: id, title: title, price: price });
+    }
+
+}
+
+function main() {
+    let prodId = document.getElementById('producto');
+    let boton = document.querySelector('button');
+
+    boton.addEventListener('click', (e) => {
+        e.preventDefault();
+        clearFields();
+        aquireDataPipe(prodId.value);
+        // try {
+        //     obtenerProducto(prodId.value);
+        // } catch (error) {
+        //     console.error(error);
+        // } finally {
+
+        // }
+    });
+}
+
+window.addEventListener('load', main());
